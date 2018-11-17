5a521d016631c180e49ce3effb9e77a042930beb
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2018-11-17 13:52:15 +0100

    Update comments which are more meaningful

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,4 @@
+# known as a shortcut, allows to place a simple command in front of a longer or less memorable command.
 alias gst='git status $*'
 alias gdf='git diff $*'
 alias gdfc='git diff --cached $*'
@@ -7,6 +8,18 @@ alias gc='git checkout $*'
 alias ga='git add $*'
 alias gm='git commit -m $*'
 alias gma='git commit --amend $*'
-#alias dn='docker network inspect cms-application -f ""{{range $id, $value := .Containers}} {{(index $value).Name}} {{end}}""'
-alias showDockerTags='function tags() { curl "https://registry.hub.docker.com/v2/repositories/library/$1/tags/" | jq "".\"results\"[][\"name\"]""; }; tags'
-#alias testPassArgs='function tags() { echo "first arg: $1"; };tags'
+
+# Display detailed information on one or more networks
+alias dn='docker network inspect cms-application -f ""{{range $id, $value := .Containers}} {{(index $value).Name}} {{end}}""'
+
+# Display versions list available in the docker hub
+alias showDockerTags='function getTags() { curl "https://registry.hub.docker.com/v2/repositories/library/$1/tags/" | jq "".\"results\"[][\"name\"]""; }; getTags'
+
+# Maven command to run a Spring Boot application.
+alias run-spring='mvn spring-boot:run'
+
+# Maven command to clean/install and then run a Spring Boot application.
+alias install-run='mvn clean install spring-boot:run'
+
+# Just test, how to invoke a function in
+alias testPassArgs='function tags() { echo "first arg: $1"; };tags'
